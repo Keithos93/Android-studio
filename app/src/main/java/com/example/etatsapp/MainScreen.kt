528535aec875b2391b1277de53974848f59f8cea
Mulamba <mulambakeith3@gmail.com>
2026-02-24 14:01:12 +0100

    etats app

---
+++ app/src/main/java/com/example/etatsapp/MainScreen.kt
@@ -0,0 +1,18 @@
+package com.example.etatsapp
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.etatsapp.ui.theme.PurpleGrey40
+
+@Composable
+fun MainScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = PurpleGrey40)
+    ) { }
+}
